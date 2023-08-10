63e656897ab3e61d3e6af0f94c61869a26109e38
Luchezarnaya79 <luchezarnaya79olga79@gmail.com>
2023-08-10 13:28:35 +0300

    Исправлены файлы

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,42 +1,50 @@
 package ru.netology.web.test;
 
+
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
-import ru.netology.web.page.LoginPageV2;
-import ru.netology.web.page.LoginPageV3;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
+
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.web.data.DataHelper.*;
 
 class MoneyTransferTest {
+
+  LoginPage loginPage;
+
+  DashboardPage dashboardPage;
+
+  @BeforeEach
+  void setup(){
+
+    loginPage = open("http://localhost:9999", LoginPage.class);
+    var authInfo = getAuthInfo();
+    var  verificationPage = loginPage.validLogin(authInfo);
+    var verificationCode = getVerificationCode(authInfo);
+    dashboardPage = verificationPage.validVerify(verificationCode);
+  }
+
     @Test
-    void shouldTransferMoneyBetweenOwnCardsV1() {
-      open("http://localhost:9999");
-      var loginPage = new LoginPageV1();
-//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
-      var authInfo = DataHelper.getAuthInfo();
-      var verificationPage = loginPage.validLogin(authInfo);
-      var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-      verificationPage.validVerify(verificationCode);
+    void shouldTransferFromFirstToSecond() {
+      var firstCardInfo = getFirstCardInfo();
+      var secondCardInfo = getSecondCardInfo();
+      var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+      var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+      var amount = generateValidAmount(firstCardBalance);
+      var expectedBalanceFirstCard = firstCardBalance - amount;
+      var expectedBalanceSecondCard = secondCardBalance + amount;
+      var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+      var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+      var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+      assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+      assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
     }
 
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV2() {
-    open("http://localhost:9999");
-    var loginPage = new LoginPageV2();
-//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
 
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV3() {
-    var loginPage = open("http://localhost:9999", LoginPageV3.class);
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
 }
 
